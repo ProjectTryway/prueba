bd7d6c5e1b9549d1de9cd61f6ea547c41ae91546
ProjectTryway <50849740+ProjectTryway@users.noreply.github.com>
2025-08-26 14:18:41 -0300

    Update config.js

--- config.js
+++ config.js
@@ -67,7 +67,7 @@ config.background_map_based = false
 // Multiple images will automatically shuffle between them
 // (Place the image files in the backgrounds/images folder)
 config.background_images = [
-	"gammawallpaper.png"
+	"gamma_logo_v2.png"
 ]
 
 // (Image-background only)
@@ -161,4 +161,5 @@ config.messages_fade_duration = 1000
 // Can be useful for finding errors
 // "true" or "false"
 config.errors_show_ingame = true
+
 
